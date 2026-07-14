e257619a9eb236edf46b3a6465905aec8994a628
PintaPontus <matteo.blascoviz@gmail.com>
2026-07-14 13:58:41 +0200

    feat(env): environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  defaultLanguage: 'it',
+  movieDBKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjk4MWI1OGNmNjIxYTkwMTQ2YjYyNDUzMTRmMjYxZCIsIm5iZiI6MTc1NjEzODAxMy45OTQsInN1YiI6IjY4YWM4YTFkNmZhN2JjM2FkYmE5YTc4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yMfw2nt-XbBEbfcdKH2mnb6uis_-tWYdwu2ho3wlv-8',
+  videoStreamingDomain: 'https://vixsrc.to',
+  firebaseConfig: {
+    apiKey: "AIzaSyD9MEQB9aOgXqRiUIm4Q5FDd6bjEpTfHMo",
+    authDomain: "pintastreaming.firebaseapp.com",
+    projectId: "pintastreaming",
+    storageBucket: "pintastreaming.firebasestorage.app",
+    messagingSenderId: "788614532075",
+    appId: "1:788614532075:web:01228e8fb6fadf938527ae",
+    measurementId: "G-X9YXPLQ2E6"
+  },
+};
