1c78bb3b989eafc3e4ea90460c1692f362aee0b6
PintaPontus <matteo.blascoviz@gmail.com>
2026-09-01 18:11:44 +0200

    feat(language-selection): add loading spinner and refactor state management
    - Replaced `ngOnInit` with `resource` for streamlined language loading and sorting.
    - Added `MatProgressSpinner` for better user feedback during loading.
    - Updated template and styles to include spinner alignment and loading state handling.

--- src/app/language-selection/language-selection.ts
+++ src/app/language-selection/language-selection.ts
@@ -1,20 +1,30 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, resource} from '@angular/core';
 import {MatListItem, MatNavList} from '@angular/material/list';
 import {MovieDBService} from '../movie-db.service';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
 import {ShowLanguage} from '../../interfaces/show';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-language-selection',
   imports: [
     MatNavList,
-    MatListItem
+    MatListItem,
+    MatProgressSpinner
   ],
   templateUrl: './language-selection.html',
   styleUrl: './language-selection.css'
 })
-export class LanguageSelection implements OnInit {
-  languages = signal([] as ShowLanguage[]);
+export class LanguageSelection {
+  languages = resource({
+    loader: async () => (await this.movieDbService.getLanguages())
+      .sort((a, b) => {
+        const nameA = a.name || a.english_name;
+        const nameB = b.name || b.english_name;
+        return nameA.localeCompare(nameB)
+      }),
+    defaultValue: [] as ShowLanguage[],
+  });
   private readonly movieDbService = inject(MovieDBService);
   private readonly bottomSheet = inject(MatBottomSheet);
 
@@ -23,14 +33,4 @@ export class LanguageSelection implements OnInit {
     this.movieDbService.setLanguage(langId)
   }
 
-  async ngOnInit() {
-    this.languages.set(
-      (await this.movieDbService.getLanguages())
-        .sort((a, b) => {
-          const nameA = a.name || a.english_name;
-          const nameB = b.name || b.english_name;
-          return nameA.localeCompare(nameB)
-        })
-    );
-  }
 }
