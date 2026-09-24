19c94bc4702bf010a79f043ceb25cbdcf87c5a52
PintaPontus <matteo.blascoviz@gmail.com>
2026-09-24 15:48:10 +0200

    fix(worker): add error handling to updateStreamList and improve type safety

--- src/worker/schedule/updateStreamList.ts
+++ src/worker/schedule/updateStreamList.ts
@@ -5,11 +5,15 @@ import {fetchGet} from '../utils/httpFetch';
 import {deleteOldShowsLists, updateShows} from '../utils/firebase/shows';
 
 export async function updateStreamList(env: Env) {
-  const streamMoviesList: ShowResource[] = await fetchGet("https://vixsrc.to/api/list/movie/?lang=it");
-  const streamTvSeriesList: ShowResource[] = await fetchGet("https://vixsrc.to/api/list/tv/?lang=it");
-  await updateShows(env, streamMoviesList, streamTvSeriesList);
-  console.log(`Show list aggiornata: ${streamMoviesList.length} film e ${streamTvSeriesList.length} serie TV`);
-  const countDeleted = await deleteOldShowsLists(env);
-  console.log(`Deleted ${countDeleted} old shows`);
+  const streamMoviesList = await fetchGet<ShowResource[]>("https://vixsrc.to/api/list/movie/?lang=it");
+  const streamTvSeriesList = await fetchGet<ShowResource[]>("https://vixsrc.to/api/list/tv/?lang=it");
+  try {
+    await updateShows(env, streamMoviesList, streamTvSeriesList);
+    console.log(`Show list aggiornata: ${streamMoviesList.length} film e ${streamTvSeriesList.length} serie TV`);
+    const countDeleted = await deleteOldShowsLists(env);
+    console.log(`Deleted ${countDeleted} old shows`);
+  } catch (error) {
+    console.error("Errore durante l'aggiornamento: ", error);
+  }
 }
 
